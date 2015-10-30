df5f51619c9ede88a357b7c1995a5c91138eba58
jasonwong115 <jason.wong115@yahoo.com>
2015-10-29 22:35:46 -0500

    Cleaning up code, and modified PriorityQueue to use a deep copy for purposes of reusing the graph for multiple traversal algorithms.

--- src/HW6/Graph.java
+++ src/HW6/Graph.java
@@ -1,20 +1,19 @@
 package HW6;
 
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
-//This class represents a directed graph using adjacency list
+//This class represents a non directed graph
+//Author: Jason Wong
 class Graph {
     private int V; // No. of vertices
+    private PriorityQueue<Integer> adj[]; // Array of priority queues for "neighbor" nodes
 
-    // Array of prioity queues for Adjacency List Representation
-    private PriorityQueue<Integer> adj[];
-
-    // Constructor
+    // Graph Constructor
     Graph(int v) {
         V = v;
         adj = new PriorityQueue [v];
+
+        // Initialize all priority queues
         for (int i = 0; i < v; ++i)
             adj[i] = new PriorityQueue<Integer>();
     }
@@ -25,8 +24,8 @@ class Graph {
         int ascii = ((int) v)-97;
         int ascii2 = ((int) w)-97;
 
-        adj[ascii].add(ascii2); // Add w to v's list.
-        adj[ascii2].add(ascii); // Add w to v's list.
+        adj[ascii].add(ascii2); // Add w to v's list since undirected
+        adj[ascii2].add(ascii); // Add w to v's list since undirected
     }
 
     // A function used by DFS
@@ -37,58 +36,58 @@ class Graph {
         int ascii = v + 97;
         char c = (char) ascii;
 
+        //Process vertex early
         System.out.print(c + " ");
 
-        PriorityQueue temp = adj[v];
+        // Make a deep copy of the queue to use as to not to destroy the graph
+        PriorityQueue temp = new PriorityQueue(adj[v]);
 
         // Recur for all the vertices adjacent to this vertex
-        while (!adj[v].isEmpty()) {
+        while (!temp.isEmpty()) {
             int n = (int) temp.remove();
             if (!visited[n])
                 DFSUtil(n, visited);
 
         }
 
+        //Process vertex late
         //System.out.print(c + " ");
 
     }
 
     // The function to do DFS traversal. It uses recursive DFSUtil()
-    void DFS(int v) {
-        // Mark all the vertices as not visited(set as
-        // false by default in java)
+    void DFS(char v) {
+        int ascii = ((int) v)-97;
+
+        // Initially mark all the vertices as not visited
         boolean visited[] = new boolean[V];
 
-        // Call the recursive helper function to print DFS traversal
-        DFSUtil(v, visited);
+        // Conduct DFS traversal
+        DFSUtil(ascii, visited);
     }
 
     public static void main(String args[]) {
-        Graph g = new Graph(27);
 
+        Graph g = new Graph(27);
         g.addEdge('a','c');
         g.addEdge('a','b');
-
         g.addEdge('b','k');
         g.addEdge('b','i');
         g.addEdge('b','d');
-
         g.addEdge('d','e');
         g.addEdge('d','i');
-
         g.addEdge('i','f');
         g.addEdge('i','g');
         g.addEdge('i','j');
-
         g.addEdge('k','j');
-
         g.addEdge('e','f');
-
         g.addEdge('g','h');
         g.addEdge('g','l');
 
         System.out.println("Following is Depth First Traversal " + "(starting from vertex a)");
+        g.DFS('a');
 
-        g.DFS(0);
+        System.out.println("\nFollowing is Depth First Traversal " + "(starting from vertex c)");
+        g.DFS('c');
     }
 }
